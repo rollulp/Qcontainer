b157e66e5f2d7b77d1782bb7a510cf78c55e12c0
Rollulp <rolando.sartorello@gmail.com>
2018-12-05 15:03:46 +0100

    Stable Container

--- a.cpp
+++ a.cpp
@@ -4,6 +4,7 @@ void print(const Container<int> &u) {
 	for(int i = 0; i < u.size(); i++)
 		cout << u[i] << endl;
 }
+
 void a() {
 Container<int> a;
 a.resize(10);
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,20 +3,38 @@
 #include "container_dao.h"
 #include<iostream>
 using namespace std;
+struct C{
+    int i;
+    int n;
+    static int f;
+    C(int i = 0) : i(i), n(f++) {
+          cout <<"new C " << n  << ' ' << this << endl;
+    }
+    ~C() {
+        cout << "del C " << n  << ' ' << this << endl;
+    }
+    C(const C&c) : C(c.i) {}
+    operator int() {return i;}
+    int& operator = (int i) {
+        this->i = i;
+        return this->i;
+    }
+};
+int C::f = 0;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    Container_DAO<int> list(16);
-    for(size_t i = 0; i < list.size(); i++)
-        list[i] = i;
+    size_t t = 0;
 
-    Container<int> list2 = list;
-    list2.delete_at(3);
-    list2.delete_at(3);
-    for(size_t i = 0; i < list2.size(); i++)
-        cout << list2[i] << endl;
+    DAO::setPath("/home/studente/Desktop/a.dat");
+    Container_DAO<C> list(t);
+    for(size_t i = 0; i < t; i++)
+        list[i] = i;
+    for(size_t i = 0; i < list.size(); i++)
+        cout << i << ": " << list[i] << endl;
+
 }
 
 MainWindow::~MainWindow()
